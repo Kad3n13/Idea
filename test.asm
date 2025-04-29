309907d58c1ab8444119bc85d7e25ffd96748e23
Kaden-Oberfeld <159424810+Kad3n13@users.noreply.github.com>
2025-04-29 11:50:09 +0000

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,97 +1,90 @@
 section .data
-nmap_cmd	db "/bin/nmap", 0
-result_file	db "nmap_result.txt", 0 
+nmap_cmd db "/bin/nmap", 0
+result_file db "nmap_result.txt", 0
 reverse_shell_cmd db "/bin/bash", 0
-search_str	db "open", 0
-file_buffer	times 256 db 0
+search_str db "open", 0
+file_buffer times 256 db 0
 file_descriptor dq 0
-target		db "111.0.0.1" 0 ; Replace
-
-;arguments for nmap
-arg0 db "nmap", 0
-arg1 db "-sS", 0
-arg2 db "-sV", 0
-arg3 db "-sC", 0
-arg4 db "-Pn", 0
-arg5 db "-p-", 0
-
-;array
-argv dq arg0, arg1, arg2, arg3, arg4, arg5, 0
-
+target db "192.168.1.1", 0  ; Example target IP
+ 
 section .text
 global _start
-
-_start
-mov rax, 59
-lea rdi, [nmap_cmd]
-lea rsi, [argv]
-xor rdx, rdx
-syscall
-
-mov rax, 2
-lea rdi, [result_file]
-mov rsi, 0
-xor rdx, rdx
-syscall
-mov [file_descriptor], rax
-
-mov rax, 0 
-mov rdi, [file_descriptor]
-lea rsi, [file_buffer]
-mov rdx, 256
-syscall
-
-lea rdi, [file_buffer]
-call check_for_open
-
-cmp rax, 1
-je service_active
-
-mov rdi, reverse_shell_cmd
-call execute_reverse_shell    ;obsfucate 
-
+ 
+_start:
+    ; Run nmap command
+    mov rax, 59
+    lea rdi, [nmap_cmd]
+    lea rsi, [target]
+    xor rdx, rdx
+    syscall
+ 
+    ; Open result file
+    mov rax, 2
+    lea rdi, [result_file]
+    mov rsi, 0
+    xor rdx, rdx
+    syscall
+    mov [file_descriptor], rax
+ 
+    ; Read from file
+    mov rax, 0
+    mov rdi, [file_descriptor]
+    lea rsi, [file_buffer]
+    mov rdx, 256
+    syscall
+ 
+    ; Check for the string "open"
+    lea rdi, [file_buffer]
+    call check_for_open
+ 
+    cmp rax, 1
+    je service_active
+ 
+    ; If "open" is not found, execute reverse shell
+    mov rdi, reverse_shell_cmd
+    call execute_reverse_shell
+ 
 service_active:
-mov rax, 60
-xor rdi, rdi
-syscall
-
+    ; Exit
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+ 
 check_for_open:
-mov rbx, "open"
-xor rcx, rcx
-
+    mov rbx, search_str  ; Address of "open"
+    xor rcx, rcx
+ 
 .loop_check:
-mov al, byte [rdi + rcx]
-cmp al, byte [rbx + rcx]
-jne .not_open
-inc rcx
-cmp byte [rbx + rcx], 0
-je .found_open
-jmp .loop_check
-
+    mov al, byte [rdi + rcx]
+    cmp al, byte [rbx + rcx]
+    jne .not_open
+    inc rcx
+    cmp byte [rbx + rcx], 0
+    je .found_open
+    jmp .loop_check
+ 
 .not_open:
-inc rcx
-cmp byte [rdi + rcx], 0
-jne .loop_check
-
-xor rax, rax
-ret
-
+    inc rcx
+    cmp byte [rdi + rcx], 0
+    jne .loop_check
+ 
+    xor rax, rax
+    ret
+ 
 .found_open:
-mov rax, 1
-ret
-
+    mov rax, 1
+    ret
+ 
 execute_reverse_shell:
-mov rax, 59
-lea rdi, [reverse_shell_cmd]
-xor rsi, rsi
-xor rdx, rdx
-syscall
-
-
+    mov rax, 59
+    lea rdi, [reverse_shell_cmd]
+    xor rsi, rsi
+    xor rdx, rdx
+    syscall
+ 
 print_strings:
-mov rax, 0x1
-mov rdi, 0
-mov rdx, 256
-syscall
-ret
-
+    mov rax, 0x1
+    mov rdi, 0
+    mov rdx, 256
+    syscall
+    ret
